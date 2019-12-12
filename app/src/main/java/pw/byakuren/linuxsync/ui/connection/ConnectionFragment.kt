68cc58ffa42eaeb54fdbcaa6e77c5cd215b4e759
Brod8362 <brodriguez410@gmail.com>
2019-12-11 18:48:38 -0600

    enable/disable buttons based on network availability

--- app/src/main/java/pw/byakuren/linuxsync/ui/connection/ConnectionFragment.kt
+++ app/src/main/java/pw/byakuren/linuxsync/ui/connection/ConnectionFragment.kt
@@ -7,6 +7,7 @@ import android.text.format.Formatter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -29,19 +30,26 @@ class ConnectionFragment : Fragment() {
         val ipView: TextView = root.findViewById(R.id.ipview)
         val ipText = getIpAddressString()
         ipView.text = getString(R.string.ip_address_text, ipText)
+        if (!affiliatedWithNetwork()) {
+            val startButton: Button = root.findViewById(R.id.start)
+            val stopButton: Button = root.findViewById(R.id.stop)
+            startButton.isEnabled = false
+            stopButton.isEnabled = false
+        }
 
 
         return root
     }
 
-    private fun setIpView() {
-
-    }
-
     private fun getIpAddressString(): String {
         val wifiManager = context?.applicationContext?.getSystemService(WIFI_SERVICE) as WifiManager
+        if (wifiManager.dhcpInfo.ipAddress == 0) return "Not affiliated"
         return Formatter.formatIpAddress(wifiManager.dhcpInfo.ipAddress)
     }
+
+    private fun affiliatedWithNetwork(): Boolean =
+        (context?.applicationContext?.getSystemService(WIFI_SERVICE) as WifiManager)
+            .dhcpInfo.ipAddress != 0
 
 
 
